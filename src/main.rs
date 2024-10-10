bd24b31982ae605af9eab906679f42accda33ac2
Stanislav <90869971+hdbg@users.noreply.github.com>
2024-10-10 13:21:10 +0200

    some changes

--- src/main.rs
+++ src/main.rs
@@ -61,17 +61,13 @@ pub mod parsers {
 
     pub struct Class {
         name: String,
-        code: Option<String>,
+        code: String,
         kind: ClassKind,
         lecturer: String,
         range: TimeRange,
         place: ClassPlace,
 
         group: Vec<Group>,
-
-        semester: Semester,
-        faculty: I18NPath,
-        degree: Degree,
     }
 
     pub mod pjatk {
@@ -121,22 +117,25 @@ pub mod parsers {
 
         mod deduct {
             use core::panic;
+            use std::str::FromStr;
 
-            use crate::parsers::{Class, ClassKind, I18NPath};
+            use chrono::{DateTime, NaiveDateTime, Utc};
+
+            use crate::parsers::{Class, ClassKind, ClassPlace, Group, I18NPath, TimeRange};
 
             use super::PjatkClass;
 
-            pub fn deduct_faculty(part: &str) -> I18NPath {
-                match part.chars().nth(1).unwrap() {
-                    'I' => "pjatk.faculties.computer_science",
-                    'K' => "pjatk.faculties.japanese_culture",
-                    'G' => "pjatk.faculties.new_media_art",
-                    'A' => "pjatk.faculties.interior_architecture",
-                    'Z' => "pjatk.faculties.management",
-                    'L' => "pjatk.common.language_studies",
-                    _ => panic!("unknown pjatk faculty"),
-                }
-            }
+            // pub fn deduct_faculty(part: &str) -> I18NPath {
+            //     match part.chars().nth(1).unwrap() {
+            //         'I' => "pjatk.faculties.computer_science",
+            //         'K' => "pjatk.faculties.japanese_culture",
+            //         'G' => "pjatk.faculties.new_media_art",
+            //         'A' => "pjatk.faculties.interior_architecture",
+            //         'Z' => "pjatk.faculties.management",
+            //         'L' => "pjatk.common.language_studies",
+            //         _ => panic!("unknown pjatk faculty"),
+            //     }
+            // }
 
             pub fn deduct_kind(class: &PjatkClass) -> ClassKind {
                 match class.kind.as_str() {
@@ -148,27 +147,59 @@ pub mod parsers {
 
             pub fn deduct_groups(class: &PjatkClass) -> Vec<Group> {
                 let raw_groups = class.groups.split(",");
-                let mut normalized_groups = Vec::new();
+                // let mut normalized_groups = Vec::new();
 
-                const GROUPS_SUFFIXES: &[char] = &['c', 'w', 'l'];
+                // const GROUPS_SUFFIXES: &[char] = &['c', 'w', 'l'];
 
-                'next_group: for raw_group in raw_groups {
-                    let this_group_parts = raw_group.split(' ').rev();
-                    for single_group_part in this_group_parts {
-                        if single_group_part.ends_with(GROUPS_SUFFIXES) {
-                            let without_suffix = &single_group_part[..single_group_part.len() - 1];
-                            if without_suffix.chars().all(|c| char::is_digit(c, 10)) {
-                                normalized_groups.push(single_group_part.trim().to_owned());
-                                continue 'next_group;
-                            }
-                        }
-                    }
+                // 'next_group: for raw_group in raw_groups {
+                //     let this_group_parts = raw_group.split(' ').rev();
+                //     for single_group_part in this_group_parts {
+                //         if single_group_part.ends_with(GROUPS_SUFFIXES) {
+                //             let without_suffix = &single_group_part[..single_group_part.len() - 1];
+                //             if without_suffix.chars().all(|c| char::is_digit(c, 10)) {
+                //                 normalized_groups.push(single_group_part.trim().to_owned());
+                //                 continue 'next_group;
+                //             }
+                //         }
+                //     }
 
-                    panic!("can't deduct pjatk group from '{}'", raw_group);
-                }
+                //     panic!("can't deduct pjatk group from '{}'", raw_group);
+                // }
 
-                return normalized_groups;
+                // return normalized_groups;
+                raw_groups.map(|x| Group { code: x.to_owned() }).collect()
             }
+
+            use chrono::TimeZone;
+            pub fn deduct_range(class: &PjatkClass) -> TimeRange {
+                let date = chrono::NaiveDate::parse_from_str(&class.date, "%d.%m.%Y").unwrap();
+                let begin_time =
+                    chrono::NaiveTime::parse_from_str(&class.from, "%H:%M:%S").unwrap();
+                let end_time = chrono::NaiveTime::parse_from_str(&class.to, "%H:%M:%S").unwrap();
+
+                let datetime = NaiveDateTime::new(date, begin_time);
+                let duration = end_time - begin_time;
+
+                let utc_time = chrono_tz::Europe::Warsaw
+                    .from_local_datetime(&datetime)
+                    .unwrap();
+                TimeRange {
+                    start: utc_time.with_timezone(&Utc),
+                    duration,
+                }
+            }
+
+            pub fn deduct_place(class: &PjatkClass) -> ClassPlace {
+                if class.is_online {
+                    ClassPlace::Online
+                } else {
+                    ClassPlace::OnSite {
+                        room: class.room.to_owned(),
+                    }
+                }
+            }
+
+            pub fn deduct_all(item: PjatkClass) -> Class {}
             pub fn multi(input: impl Iterator<Item = PjatkClass>) -> Vec<Class> {}
         }
 
@@ -182,6 +213,7 @@ pub mod parsers {
             pub room: String,
             pub from: String,
             pub to: String,
+            pub date: String,
 
             // temporary value used for resolving if class is online
             pub is_online: bool,
@@ -239,6 +271,7 @@ pub mod parsers {
                 const LECTURER: &str = "#ctl06_DydaktycyLabel";
 
                 const ROOM_SELECTOR: &str = "#ctl06_SalaLabel";
+                const DATE_SELECTOR: &str = "#ctl06_DataZajecLabel";
 
                 const FROM_TIME_SELECTORS: &str = "#ctl06_GodzRozpLabel";
                 const TO_TIME_SELECTORS: &str = "#ctl06_GodzZakonLabel";
@@ -266,6 +299,7 @@ pub mod parsers {
                     from: parse_selector!(document, FROM_TIME_SELECTORS),
                     to: parse_selector!(document, TO_TIME_SELECTORS),
                     is_online: code.contains(ONLINE_COLOR_SUBSTR),
+                    date: parse_selector!(document, DATE_SELECTOR),
                 }))
             }
 
